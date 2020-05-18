e1d6ef97f9881f37c3780e423be940c042d212f8
Silvio Oliveira <Silvio.Oliveira@net-a-porter.com>
2020-05-18 01:10:15 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to FibCalculator V2</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <Route exact path="/" component={Fib} />
+        <Route exact path="/otherpage" component={OtherPage} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
