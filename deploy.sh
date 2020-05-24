2eab5cd7090cf4d8a1490df6c954bfb6a21dcbfb
Silvio Oliveira <Silvio.Oliveira@net-a-porter.com>
2020-05-24 01:33:39 +0100

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ docker push silviooliveira/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set images deployments/server-deployment server=silviooliveira/multi-server:$SHA
-kubectl set images deployments/client-deployment client=silviooliveira/multi-client:$SHA
-kubectl set images deployments/worker-deployment worker=silviooliveira/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=silviooliveira/multi-server:$SHA
+kubectl set image deployments/client-deployment client=silviooliveira/multi-client:$SHA
+kubectl set image  deployments/worker-deployment worker=silviooliveira/multi-worker:$SHA
